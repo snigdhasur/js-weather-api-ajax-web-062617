7e47dd3283e189b78b696912f14be65fe4fa934b
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-08-16 14:10:11 -0400

    Done.

--- js/init.js
+++ js/init.js
@@ -1,10 +1,24 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+  var API_KEY = "947275bedd639909";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
-  makeAjaxRequest(URL, function(json) {
+ document.getElementById('chartArea').innerHTML = makeAjaxRequest(URL, function(json) {
     var data = generateDataSet(getHours(json), getFahrenheits(json));
-    var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+    var tempChart = new Chart(ctx, { type: 'line', data: data, options: { bezierCurve: true }});
   });
+
+// document.getElementById('chartArea').innerHTML = makeAjaxRequest(URL, function(json) {
+//     var data = generateDataSet(getHours(json), getFahrenheits(json));
+//     var tempChart = new Chart(ctx).Line(data, { bezierCurve: true });
+//   });
+
+
+ //  var result = $.ajax({
+	// 	  url: "http://api.wunderground.com/api/947275bedd639909/hourly/q/NY/New_York.json",
+	// 	  type: 'GET',
+	// 	  success: function(data){
+	// 	  	console.log(data)
+	// 	  }
+	// })
 });
--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,32 @@
 function getFahrenheits(result){
-  // Your code goes here
+	return result.hourly_forecast.map(r => r.temp.english)
 }
 
 function getHours(result){
-  // Your code goes here
+  return result.hourly_forecast.map(r => r.FCTTIME.hour)
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  return {labels: labels,
+      datasets: [
+      {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: data
+      }
+      ]
+  }
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+   $.ajax({
+		  url: endpoint,
+		  dataType: 'jsonp',
+		  success:  success
+	})
 }
